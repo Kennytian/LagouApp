f39d6bf7a2de8cfb4821a460be4b5e281bf7efa9
Kennytian <kenny_tian@outlook.com>
2016-09-02 20:32:19 +0800

    remove unuse files

--- launcher.js
+++ launcher.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {AppRegistry, Navigator} from 'react-native';
 
 import Splash from './src/splash';
-//import Frame from './src/frame';
 
 class Launcher extends Component {
 	render() {
@@ -10,10 +9,10 @@ class Launcher extends Component {
 		let defaultComponent = Splash;
 		return (
 			<Navigator initialRoute={{name: defaultName, component: defaultComponent}}
-			           renderScene={(route, navigator) => {
-				           let Component = route.component;
-				           return <Component {...route.params} navigator={navigator} />
-			           }}
+         renderScene={(route, navigator) => {
+           let Component = route.component;
+           return <Component {...route.params} navigator={navigator} />
+         }}
 			/>
 		)
 	}
--- splash.js
+++ splash.js
@@ -3,39 +3,39 @@
  */
 'use strict';
 
-import React, { Component, } from 'react';
+import React, {Component,} from 'react';
 import React, {
-  StyleSheet, Image,
+	StyleSheet, Image,
 } from 'react-native';
 
 import FramePage from './framepage';
 
 export default class SplashScreen extends Component {
-  constructor(props) {
-    super(props);
-    var { navigator } = props;
-    setTimeout(() => {
-      navigator.replace({
-        name      : 'FramePage',
-        component : FramePage
-      })
-    }, 1000);
-  }
+	constructor(props) {
+		super(props);
+		var {navigator} = props;
+		setTimeout(() => {
+			navigator.replace({
+				name: 'FramePage',
+				component: FramePage
+			})
+		}, 1000);
+	}
 
-  render() {
-    return (
-      <Image source={require('./images/hello_page_bg.png')} style={styles.backgroundImage} />
-    );
-  }
+	render() {
+		return (
+			<Image source={require('./images/hello_page_bg.png')} style={styles.backgroundImage}/>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  backgroundImage : {
-    flex           : 1,
-    justifyContent : 'center',
-    alignItems     : 'center',
-    width          : null,
-    height         : null,
-    resizeMode     : 'cover'
-  }
+	backgroundImage: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: null,
+		height: null,
+		resizeMode: 'cover'
+	}
 });
